dda965e5d1292e99306b221d5ef5589ea3a2b5d3
Prashan Raj KC <prashanraj.kc@aria.com.np>
2024-06-27 10:55:57 +0545

    Add project files.

---
+++ HRLeaveManagemnetDomain/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace HRLeaveManagemnetDomain;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
